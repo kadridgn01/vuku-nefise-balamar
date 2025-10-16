7d89f10b05f86683b56eedc881f9d4d091869263
kadridgn01 <projectsartmov@gmail.com>
2025-10-16 11:43:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,25 @@
-// Bulucu PWA - v1
-const CACHE_NAME = "bulucu-v1";
+// Vuku PWA - v2
+const CACHE_NAME = "vuku-v2";
 const urlsToCache = [
     "./",
     "./index.html",
     "./manifest.json",
-    "./search-data.json?v=1"
+    "./search-data.json?v=2"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
-                          .map(cacheName => caches.delete(cacheName))
-            );
-        })
-    );
+    event.waitUntil(caches.keys().then(cacheNames => Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+                  .map(cacheName => caches.delete(cacheName))
+    )));
     return self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
