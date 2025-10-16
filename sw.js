5980d517be62010fe99fac29d6b28c82dd7b2cf8
kadridgn01 <projectsartmov@gmail.com>
2025-10-16 11:32:21 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+// Bulucu PWA - v1
+const CACHE_NAME = "bulucu-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./search-data.json?v=1"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+                          .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
